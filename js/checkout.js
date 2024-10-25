a3290fd521185e32b47b0632d3d626c5243f3c1e
LaraBerenguer <laraberenguer@outlook.com>
2024-10-25 16:22:14 +0200

    Finished validate() function.

--- js/checkout.js
+++ js/checkout.js
@@ -5,24 +5,64 @@ function validate() {
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	var errorEmail = document.getElementById("errorEmail");
+	var errorAddress = document.getElementById("errorAddress");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorPhone = document.getElementById("errorPhone");
+
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	let lettersAndSpace = /[a-zA-Z ]/gm;
+	let numbers = /[0-9]/gm;
+	let numbersAndLetters = /[a-zA-Z0-9]/gm;
+
+	//Name
+	if (fName.value == "" || fName.value.length < 3 || !fName.value.match(lettersAndSpace)) {
 		error++;
+		fName.classList.add("is-invalid");
 	}
 
-	if(fEmail.value == ""){
+	//Surname
+	if (fLastN.value == "" || fLastN.value.length < 3 || !fLastN.value.match(lettersAndSpace)) {
 		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
+		fLastN.classList.add("is-invalid");		
 	}
 
+	//Mail
+	if (fEmail.value == "" || fEmail.value.length < 3) {
+		error++;
+		fEmail.classList.add("is-invalid");		
+	}
+
+	//Address
+	if (fAddress.value == "") {
+		error++;
+		fAddress.classList.add("is-invalid");		
+	}
+
+	//Password
+	if (fPassword.value == "" || fPassword.value.length < 4 || fPassword.value.length > 8 || !fPassword.value.match(numbersAndLetters)) {
+		error++;
+		fPassword.classList.add("is-invalid");		
+	}
+
+	//Phone
+	if (fPhone.value == "" || fPhone.value.length < 3 || !fPhone.value.match(numbers)) {
+		error++;
+		fPhone.classList.add("is-invalid");		
+	}
+
+	//Errors	
+	if (error > 0) {
+		alert("Please fix the errors in the form.");
+	} else {
+		alert("All good! Submiting form.")
+	}
 }
